0f91be92b1ade7e8a7cc92f6ab3342e741d47d36
sindremel07 <144103231+sindremel07@users.noreply.github.com>
2024-10-23 08:41:46 +0200

    Add project files.

---
+++ lotto app/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LottoConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int forsok = 0;
+
+            int[] lottoLapp = new int[7];
+            int[] lottoLappUnsorted = new int[7];
+
+            Console.WriteLine("Generer Random Lotto Lapp: 1 | Skriv Din Egen (7 Tall): 2");
+            string answer = Console.ReadLine();
+
+            if (answer == "1")
+            {
+                for (int i = 0; i < lottoLapp.Length; i++)
+                {
+                    int nyttTall = random.Next(1, 35);
+
+                    if (FinnNummer(nyttTall, lottoLapp))
+                    {
+                        i--;
+                    }
+                    else
+                    {
+                        lottoLapp[i] = nyttTall;
+                        lottoLappUnsorted[i] = nyttTall;
+                    }
+                }
+            }
+            else if (answer == "2")
+            {
+                Console.WriteLine("Skriv Din Egen Lotto Lapp, Det MÅ være 7 UNIKE tall og være mellom 1-35");
+                HashSet<int> tallSett = new HashSet<int>();
+
+                for (int i = 0; i < 7; i++)
+                {
+                    bool gyldigTall = false;
+
+                    while (!gyldigTall)
+                    {
+                        Console.WriteLine($"Skriv tall nummer {i + 1}:");
+                        string input = Console.ReadLine();
+
+                        if (int.TryParse(input, out int tall))
+                        {
+                            if (tall < 1 || tall > 35)
+                            {
+                                Console.WriteLine("Tallet må være mellom 1 og 35.");
+                            }
+                            else if (!tallSett.Add(tall))
+                            {
+                                Console.WriteLine("Tallet er allerede skrevet. Du må skrive et unikt tall.");
+                            }
+                            else
+                            {
+                                lottoLapp[i] = tall;
+                                lottoLappUnsorted[i] = tall;
+                                gyldigTall = true;
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Du må skrive et gyldig tall.");
+                        }
+                    }
+                }
+            }
+            else if (answer != "1" && answer != "2")
+            {
+                Console.WriteLine("Ikke et gyldig valg.");
+                return;
+            }
+
+            for (; ; )
+            {
+                forsok++;
+
+                int[] lottoTall = new int[7];
+                for (int i = 0; i < lottoTall.Length; i++)
+                {
+                    int nyttTall = random.Next(1, 35);
+
+                    if (FinnNummer(nyttTall, lottoTall))
+                    {
+                        i--;
+                    }
+                    else
+                    {
+                        lottoTall[i] = nyttTall;
+                    }
+                }
+
+                if (ArraySjekk(lottoLapp, lottoTall))
+                {
+                    Console.WriteLine("\n\n----------------------------------------------------------");
+                    Console.WriteLine("\n\nDet tok " + forsok + " forsøk for å få riktig lottolapp!");
+                    Console.WriteLine("\n\nDin lottolapp: " + string.Join(", ", lottoLappUnsorted));
+                    Console.WriteLine("Systemets lottotall: " + string.Join(", ", lottoTall));
+                    break;
+                }
+            }
+        }
+
+        public static bool FinnNummer(int userNum, int[] lottoNum)
+        {
+            foreach (int num in lottoNum)
+            {
+                if (num == userNum)
+                    return true;
+            }
+            return false;
+        }
+
+        public static bool ArraySjekk(int[] array1, int[] array2)
+        {
+            if (array1.Length != array2.Length)
+                return false;
+
+            Array.Sort(array1);
+            Array.Sort(array2);
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+                if (array1[i] != array2[i])
+                    return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ lotto app/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("lotto app")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("lotto app")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6f2a0541-c34a-4dbd-b029-cc0bd48e1a32")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
